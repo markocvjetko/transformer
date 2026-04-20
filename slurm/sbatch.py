67091d518758022c6dd58502757aef5133faaa7c
markocvjetko <marko.cvjetko1@gmail.com>
2026-04-20 18:10:27 +0200

    update

--- slurm/sbatch.py
+++ slurm/sbatch.py
@@ -3,7 +3,6 @@ import subprocess, sys, datetime, pathlib, os
 
 SBATCH = pathlib.Path(__file__).parent / "train.sbatch"
 
-cmd = [..., str(SBATCH)]
 exp_name = sys.argv[1] if len(sys.argv) > 1 else "run"
 stamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 exp_dir = pathlib.Path(os.environ["SCRATCH"]) / "transformer/experiments"# / f"{exp_name}-{stamp}"
@@ -16,6 +15,6 @@ cmd = [
     f"--output={log_dir}/%j.out",
     f"--error={log_dir}/%j.err",
     f"--export=ALL,EXP_DIR={exp_dir}",   # pass path into the job's env
-    "slurm/train.sbatch",
+    str(SBATCH),
 ]
 subprocess.run(cmd, check=True)
